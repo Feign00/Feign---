d51d467380f400b3b73dba08f19d88b3006ff43b
feign <2225744553@qq.com>
2025-02-16 02:28:22 +0800

    refactor: 删除 Feign 项目相关文件
    - 移除 Feign.png 文件
    - 删除 featured-project.html、index.html 和 modal.css 文件
    - 删除 script.js 文件

---
+++ views/ProjectDetail.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="featured-project-details">
+        <h1>主项目标题</h1>
+        <img src="project-image.jpg" alt="项目图片" class="project-image">
+        <p>这里是主项目的详细描述，可以包括以下内容：</p>
+        <ul>
+            <li>项目背景</li>
+            <li>项目目标</li>
+            <li>项目特点</li>
+            <li>技术栈</li>
+            <li>项目成果</li>
+        </ul>
+        <p>更多信息可以在这里补充。</p>
+    </div>
+</template>
+
+<script>
+export default {
+    // 可以在这里添加组件逻辑
+};
+</script>
